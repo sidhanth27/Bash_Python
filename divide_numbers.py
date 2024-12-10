e12affae543863069645d012d1b07fef5765aa85
sidka <sidka@AUTOOQfHMe1G3wo>
2024-12-10 16:23:25 +0530

    Initial commit on new branch

---
+++ divide_numbers.py
@@ -0,0 +1,7 @@
+import sys
+numerator = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+denominator = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+result = numerator // denominator
+print(f"Numerator: {numerator}")
+print(f"Denominator: {denominator}")
+print(f"Result: {result}")
